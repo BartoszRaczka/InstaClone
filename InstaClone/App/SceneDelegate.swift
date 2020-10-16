8335cb3dbb6bc28db6f902d142c8c068baec36fb
Rafał <rafal.malczynski@gmail.com>
2020-10-16 16:47:39 +0200

    Add initial project setup

---
+++ InstaClone/App/SceneDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  SceneDelegate.swift
+//  InstaClone
+//
+//  Created by MacBook on 05/10/2020.
+//  Copyright © 2020 Bartosz Raczka Development. All rights reserved.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+    var coordinator: Coordinator?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard
+            let windowScene = (scene as? UIWindowScene),
+            let firstWindow = windowScene.windows.first
+        else { return }
+        
+        coordinator = AppCoordinator(with: firstWindow)
+        coordinator?.start()
+    }
+
+}
+
---
+++ InstaClone/DependencyManagement/DependencyContainer.swift
@@ -0,0 +1,13 @@
+//
+//  DependencyContainer.swift
+//  InstaClone
+//
+//  Created by Rafał Małczyński on 16/10/2020.
+//  Copyright © 2020 Bartosz Raczka Development. All rights reserved.
+//
+
+final class DependencyContainer {
+    
+    
+    
+}
