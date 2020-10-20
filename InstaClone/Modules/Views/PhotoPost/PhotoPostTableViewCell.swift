5e621a786ee64ac2c8dfadd2fe22d7ed7da427c6
Smallfish <piotr.rybka.pl@gmail.com>
2020-10-20 14:50:23 +0200

    [IC - PhotoPost Module] do some views in tableViewCell

---
+++ InstaClone/Modules/Views/PhotoPost/PhotoPostTableViewCell.swift
@@ -0,0 +1,9 @@
+//
+//  PhotoPostTableViewCell.swift
+//  InstaClone
+//
+//  Created by new on 20/10/2020.
+//  Copyright © 2020 Bartosz Raczka Development. All rights reserved.
+//
+
+import Foundation
---
+++ InstaClone/Modules/Views/PhotoPost/PhotoPostView.swift
@@ -0,0 +1,9 @@
+//
+//  PhotoPostView.swift
+//  InstaClone
+//
+//  Created by new on 18/10/2020.
+//  Copyright © 2020 Bartosz Raczka Development. All rights reserved.
+//
+
+import Foundation
