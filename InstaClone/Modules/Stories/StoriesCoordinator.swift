c45deb39de49383b1b06b9069d7167d710949740
Bartosz Rączka <barteksae@gmail.com>
2020-10-20 22:25:30 +0200

    [IC-StoriesCoordinator] Add storiesCoordinator

--- InstaClone/Modules/Stories/StoriesCoordinator.swift
+++ InstaClone/Modules/Stories/StoriesCoordinator.swift
@@ -2,12 +2,33 @@
 //  StoriesCoordinator.swift
 //  InstaClone
 //
-//  Created by MacBook on 18/10/2020.
+//  Created by MacBook on 20/10/2020.
 //  Copyright © 2020 Bartosz Raczka Development. All rights reserved.
 //
 
 import Foundation
+import UIKit
 
-final class StoriesCoordinator {
+final class StoriesCoordinator: Coordinator {
     
+//   MARK: - Properties
+    
+    private let navigationController: UINavigationController
+    private let dependencyContainer: DependencyContainer
+    private var coordinators = [Coordinator]()
+
+//    MARK: - Life Cycle
+    
+    init(
+        with navigationController: UINavigationController,
+        dependencyContainer: DependencyContainer
+    ) {
+        self.navigationController = navigationController
+        self.dependencyContainer = dependencyContainer
+    }
+    
+//  MARK: - Public Methods
+    func start() {
+        
+    }
 }
