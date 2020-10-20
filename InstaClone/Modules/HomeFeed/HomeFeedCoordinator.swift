e79540acabaecdfa2140c77afe856e97405834d5
Bartosz Rączka <barteksae@gmail.com>
2020-10-20 15:20:37 +0200

    [IC-HomeFeed] add HomeFeedCoordinator

---
+++ InstaClone/Modules/HomeFeed/HomeFeedCoordinator.swift
@@ -0,0 +1,35 @@
+//
+//  HomeFeedCoordinator.swift
+//  InstaClone
+//
+//  Created by MacBook on 20/10/2020.
+//  Copyright © 2020 Bartosz Raczka Development. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+final class HomeFeedCoordinator: Coordinator {
+    
+    // MARK: - Properties
+    
+    private let navigationController: UINavigationController
+    private let dependencyContainer: DependencyContainer
+    private var coordinators = [Coordinator]()
+    
+    // MARK: - Life Cycle
+    
+    init(
+        with navigationController: UINavigationController,
+        dependencyContainer: DependencyContainer
+    ){
+        self.navigationController = navigationController
+        self.dependencyContainer = dependencyContainer
+    }
+    
+    // MARK: - Public Methods
+    func start() {
+        
+    }
+}
