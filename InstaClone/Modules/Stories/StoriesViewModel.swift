ba22eaf427e66cd0b69070191d54214471e94e4b
Bartosz Rączka <barteksae@gmail.com>
2020-10-22 13:54:39 +0200

    [IC-1] Add StoriesView, StoriesViewController, StoriesViewModel

---
+++ InstaClone/Modules/Stories/StoriesViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  StoriesViewModel.swift
+//  InstaClone
+//
+//  Created by MacBook on 22/10/2020.
+//  Copyright © 2020 Bartosz Raczka Development. All rights reserved.
+//
+
+import Foundation
