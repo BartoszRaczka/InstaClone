3b4ff57398b5a8287b0c3656b9d21c8cad7ee558
Bartosz Rączka <barteksae@gmail.com>
2020-10-22 13:48:07 +0200

    [IC-4] Add HomeFeedView

--- InstaClone/Modules/HomeFeed/HomeFeedView.swift
+++ InstaClone/Modules/HomeFeed/HomeFeedView.swift
@@ -68,19 +68,81 @@ final class HomeFeedView: UIView {
     }
     
     private func setupInstaCloneImage() {
-        
+//        TODO: Przygotować logo
     }
 // MARK: -Bottom Container
     
     private func setupBottomContainer() {
+        bottomContainer = UIView()
+        addSubview(bottomContainer)
         
+        bottomContainer.snp.makeConstraints{ (make) in
+            make.bottom.left.right.equalToSuperview()
+            make.height.equalTo(32)
+        }
+    }
+    
+    private func setupHomeButton() {
+        homeButton = UIButton()
+        addSubview(homeButton)
+        
+        homeButton.snp.makeConstraints{ (make) in
+            make.top.bottom.left.equalTo(bottomContainer)
+        }
+    }
+    
+    private func setupSearchbutton() {
+        searchButton = UIButton()
+        addSubview(searchButton)
+        
+        searchButton.snp.makeConstraints{ (make) in
+            make.top.bottom.equalTo(bottomContainer)
+            make.left.equalTo(homeButton)
+        }
+    }
+    
+    private func setupAddButton() {
+        addButton = UIButton()
+        addSubview(addButton)
+        
+        addButton.snp.makeConstraints{ (make) in
+            make.top.bottom.equalTo(bottomContainer)
+            make.left.equalTo(searchButton)
+        }
+    }
+    
+    private func setupActivityButton() {
+        activityButton = UIButton()
+        addSubview(activityButton)
+        
+        activityButton.snp.makeConstraints{ make in
+            make.top.bottom.equalTo(bottomContainer)
+            make.left.equalTo(addButton)
+        }
+    }
+    
+    private func setupProfileButton() {
+        profileButton = UIButton()
+        addSubview(profileButton)
+        
+        profileButton.snp.makeConstraints{(make) in
+            make.bottom.top.equalTo(bottomContainer)
+            make.left.equalTo(activityButton)
+            make.right.equalTo(bottomContainer)
+        }
     }
 // MARK: -Button Targets
     
     private func setupView() {
         photoButton.addTarget(self, action: #selector(didTapPhotoButton), for: .touchUpInside)
         dmButton.addTarget(self, action: #selector(didTapDMButton), for: .touchUpInside)
+        homeButton.addTarget(self, action: #selector(didTapHomeButton), for: .touchUpInside)
+        searchButton.addTarget(self, action: #selector(didTapSearchButton), for: .touchUpInside)
+        addButton.addTarget(self, action: #selector(didTapAddButton), for: .touchUpInside)
+        activityButton.addTarget(self, action: #selector(didTapActivityButton), for: .touchUpInside)
+        profileButton.addTarget(self, action: #selector(didTapProfileButton), for: .touchUpInside)
     }
+    
     
     @objc func didTapPhotoButton() {
 // TODO: zrobić logikę funkcji - widok mówi do modelu a model do coordinatora
@@ -88,5 +150,25 @@ final class HomeFeedView: UIView {
     
     @objc func didTapDMButton() {
 // TODO: j.w.
+    }
+    
+    @objc func didTapHomeButton() {
+    
+    }
+    
+    @objc func didTapSearchButton() {
+    
+    }
+    
+    @objc func  {
+    
+    }
+    
+    @objc func {
+    
+    }
+    
+    @objc func {
+    
     }
 }
