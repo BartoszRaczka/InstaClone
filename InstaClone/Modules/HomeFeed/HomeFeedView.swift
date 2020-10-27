b63d832050e7c986f4cb2ed7b4bbaaae45100716
Bartosz Rączka <barteksae@gmail.com>
2020-10-27 08:51:28 +0100

    [IC-5] add PR changes

--- InstaClone/Modules/HomeFeed/HomeFeedView.swift
+++ InstaClone/Modules/HomeFeed/HomeFeedView.swift
@@ -20,12 +20,6 @@ final class HomeFeedView: UIView {
     var dmButton: UIButton!
     var instaCloneImage: UIImageView!
     
-    var homeButton: UIButton!
-    var searchButton: UIButton!
-    var addButton: UIButton!
-    var activityButton: UIButton!
-    var profileButton: UIButton!
-    
     init(with viewModel: HomeFeedViewModel){
         self.viewModel = viewModel
         super.init(frame: .zero)
@@ -33,9 +27,11 @@ final class HomeFeedView: UIView {
     }
     
      required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        nil
     }
+    
     // MARK: -Top Container
+    
     private func setupTopContainer() {
         topContainer = UIView()
         addSubview(topContainer)
@@ -68,8 +64,9 @@ final class HomeFeedView: UIView {
     }
     
     private func setupInstaCloneImage() {
-        // TODO: Przygotować logo
+        // TODO: Make a logo image
     }
+    
     // MARK: -Bottom Container
     
     private func setupBottomContainer() {
@@ -81,94 +78,7 @@ final class HomeFeedView: UIView {
             make.height.equalTo(32)
         }
     }
-    
-    private func setupHomeButton() {
-        homeButton = UIButton()
-        addSubview(homeButton)
-        
-        homeButton.snp.makeConstraints{ (make) in
-            make.top.bottom.left.equalTo(bottomContainer)
-        }
-    }
-    
-    private func setupSearchbutton() {
-        searchButton = UIButton()
-        addSubview(searchButton)
-        
-        searchButton.snp.makeConstraints{ (make) in
-            make.top.bottom.equalTo(bottomContainer)
-            make.left.equalTo(homeButton)
-        }
-    }
-    
-    private func setupAddButton() {
-        addButton = UIButton()
-        addSubview(addButton)
-        
-        addButton.snp.makeConstraints{ (make) in
-            make.top.bottom.equalTo(bottomContainer)
-            make.left.equalTo(searchButton)
-        }
-    }
-    
-    private func setupActivityButton() {
-        activityButton = UIButton()
-        addSubview(activityButton)
-        
-        activityButton.snp.makeConstraints{ make in
-            make.top.bottom.equalTo(bottomContainer)
-            make.left.equalTo(addButton)
-        }
-    }
-    
-    private func setupProfileButton() {
-        profileButton = UIButton()
-        addSubview(profileButton)
-        
-        profileButton.snp.makeConstraints{(make) in
-            make.bottom.top.equalTo(bottomContainer)
-            make.left.equalTo(activityButton)
-            make.right.equalTo(bottomContainer)
-        }
-    }
-    // MARK: -Button Targets
-    
-    private func setupView() {
-        photoButton.addTarget(self, action: #selector(didTapPhotoButton), for: .touchUpInside)
-        dmButton.addTarget(self, action: #selector(didTapDMButton), for: .touchUpInside)
-        homeButton.addTarget(self, action: #selector(didTapHomeButton), for: .touchUpInside)
-        searchButton.addTarget(self, action: #selector(didTapSearchButton), for: .touchUpInside)
-        addButton.addTarget(self, action: #selector(didTapAddButton), for: .touchUpInside)
-        activityButton.addTarget(self, action: #selector(didTapActivityButton), for: .touchUpInside)
-        profileButton.addTarget(self, action: #selector(didTapProfileButton), for: .touchUpInside)
-    }
-    
-    
-    @objc func didTapPhotoButton() {
-        // TODO: make a method - view communicate with a model and then to the coordinator
-    }
-    
-    @objc func didTapDMButton() {
-    
-    }
-    
-    @objc func didTapHomeButton() {
-    
-    }
-    
-    @objc func didTapSearchButton() {
-    
-    }
-    
-    @objc func  didTapAddButton() {
-    
-    }
-    
-    @objc func didTapActivityButton() {
-    
-    }
-    
-    @objc func didTapProfileButton() {
-    
-    }
+  
 }
+    
+   
