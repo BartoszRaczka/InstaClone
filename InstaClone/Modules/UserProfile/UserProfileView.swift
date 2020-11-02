d60e66733d1ff1270ec38d446837e3dfca75ce2e
Bartosz Rączka <barteksae@gmail.com>
2020-11-02 23:38:20 +0100

    [IC-11] Create views for UserProfile module

--- InstaClone/Modules/UserProfile/UserProfileView.swift
+++ InstaClone/Modules/UserProfile/UserProfileView.swift
@@ -11,12 +11,34 @@ final class UserProfileView: UIView {
     
     var viewModel: UserProfileViewModel
     
+    private var profileButton: UIButton!
+    
     init(with viewModel: UserProfileViewModel) {
         self.viewModel = viewModel
         super.init(frame: .zero)
+        setupView()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    private func setupProfileButton() {
+            profileButton = UIButton()
+            addSubview(profileButton)
+            
+            profileButton.snp.makeConstraints{ make in
+                make.edges.equalToSuperview()
+            }
+    }
+    
+    private func setupView() {
+        setupProfileButton()
+        profileButton.addTarget(self, action: #selector(didTapProfileButton), for: .touchUpInside)
+    }
+
+    @objc func didTapProfileButton() {
+        
+    }
+    
 }
