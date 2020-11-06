4b62cdd8d9bffe5941bbe6b93427531c0f47f6bf
Smallfish <piotr.rybka.pl@gmail.com>
2020-11-06 20:03:48 +0100

    [IC-15] Add factories for stories module and modify StoriesViewCoordinator an StoriesViewModel

---
+++ InstaClone/DependencyManagement/Factories/Stories/StoriesViewControllerFactory.swift
@@ -0,0 +1,8 @@
+//
+//  StoriesViewControllerFactory.swift
+//  InstaClone
+//
+//  Created by new on 06/11/2020.
+//
+
+import Foundation
---
+++ InstaClone/DependencyManagement/Factories/Stories/StoriesViewModelFactory.swift
@@ -0,0 +1,8 @@
+//
+//  StoriesViewModelFactory.swift
+//  InstaClone
+//
+//  Created by new on 06/11/2020.
+//
+
+import Foundation
