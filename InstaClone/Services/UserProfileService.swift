b9bf2aef7cb99f5ccd281d3f53e676b7031d3f7d
Smallfish <piotr.rybka.pl@gmail.com>
2020-11-19 01:38:26 +0100

    [IC-32] Add basic UserProfileService logic

--- InstaClone/Services/UserProfileService.swift
+++ InstaClone/Services/UserProfileService.swift
@@ -7,11 +7,37 @@
 //
 
 import Foundation
+import Firebase
 
 protocol UserProfileServiceProtocol {
-    
+    func getData(user: UserData)
+    func setData(user: UserData)
 }
 
 final class UserProfileService: UserProfileServiceProtocol {
+        
+    func getData(user: UserData) {
+        reference.child("users/\(user.id)").observeSingleEvent(of: .value) { [weak self] snapshot in
+            guard
+                let self = self,
+                JSONSerialization.isValidJSONObject(snapshot.value as Any),
+                let data = try? JSONSerialization.data(withJSONObject: snapshot.value as Any),
+                let user = try? JSONDecoder().decode(User.self, from: data)
+            else {
+                return
+            }
+            // rest of the logic
+        }
+    }
+    
+    func setData(user: UserData) {
+        let userData = try! JSONEncoder().encode(user)
+        let userJSON = try! JSONSerialization.jsonObject(with: userData, options: [])
+
+        let reference = Database.database().reference(withPath: "users/\(user.id)")
+        reference.setValue(userJSON) { error, reference in
+            // rest of the logic
+        }
+    }
     
 }
