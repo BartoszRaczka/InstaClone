9fd898b283975143eff37d4bfb027744e4dc20c4
Smallfish <piotr.rybka.pl@gmail.com>
2020-11-26 01:53:10 +0100

    [IC-38] Add ProfilePageViewController

--- InstaClone/Modules/Onboarding/OnboardingViewController.swift
+++ InstaClone/Modules/Onboarding/OnboardingViewController.swift
@@ -5,8 +5,24 @@
 //  Created by new on 25/11/2020.
 //
 
-import Foundation
+import SnapKit
 
-class OnboardingViewController {
+final class OnboardingViewController: UIViewController {
+    
+    private let viewModel: OnboardingViewModel
+    
+    init(with viewModel: OnboardingViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        nil
+    }
+    
+    override func loadView() {
+        super.loadView()
+        view = OnboardingView(with: self.viewModel)
+    }
     
 }
