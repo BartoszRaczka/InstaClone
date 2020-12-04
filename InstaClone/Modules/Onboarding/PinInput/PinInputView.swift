913cb3c1043a76d1971ff461cfed33db05d44a32
Bartosz Rączka <barteksae@gmail.com>
2020-12-04 15:33:45 +0100

    [IC-48] add first part of PinInputView

--- InstaClone/Modules/Onboarding/PinInput/PinInputView.swift
+++ InstaClone/Modules/Onboarding/PinInput/PinInputView.swift
@@ -5,8 +5,66 @@
 //  Created by new on 25/11/2020.
 //
 
-import Foundation
+import SnapKit
 
-class PinInputView {
+final class PinInputView: UIView {
+    
+    // MARK: - Properties
+    
+    private let viewModel: PinInputViewModel
+    
+    private var textLabel: UILabel!
+    private var textField: UITextField!
+    
+    // MARK: - Life Cycle
+    
+    init(with viewModel: PinInputViewModel) {
+        self.viewModel = viewModel
+        super.init(frame: .zero)
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        nil
+    }
+    
+    // MARK: - Public methods
+    
+    
+    
+    // MARK: - View setup
+    
+    func setupTextLabel() {
+        textLabel = UILabel()
+        addSubview(textLabel)
+        
+        textLabel.text = "You will receive SMS with your pin number and enter it below."
+        textLabel.textColor = .white
+        textLabel.textAlignment = .center
+        
+        textLabel.snp.makeConstraints { (make) in
+            make.leading.trailing.top.equalToSuperview()
+        }
+    }
+    
+    func setupTextField() {
+        textField = UITextField()
+        addSubview(textField)
+        
+        textField.placeholder = "Enter your pin number here"
+        textField.borderStyle = .roundedRect
+        textField.autocorrectionType = .no
+        textField.keyboardType = .phonePad
+        textField.returnKeyType = .done
+        textField.clearButtonMode = .whileEditing
+        textField.contentVerticalAlignment = .center
+        textField.contentHorizontalAlignment = .leading
+        
+        textField.snp.makeConstraints { (make) in
+            make.leading.trailing.equalToSuperview()
+            make.top.equalTo(textLabel)
+            make.centerY.equalToSuperview()
+        }
+    }
     
 }
