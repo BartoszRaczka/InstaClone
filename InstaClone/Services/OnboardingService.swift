e72249220bf1c91f6058ea99ce784258bdcc692e
Smallfish <piotr.rybka.pl@gmail.com>
2020-12-08 21:27:47 +0100

    [IC-35] Add "import FirebaseAuth"

--- InstaClone/Services/OnboardingService.swift
+++ InstaClone/Services/OnboardingService.swift
@@ -6,16 +6,38 @@
 //
 
 import Foundation
+import Firebase
+import FirebaseAuth
 
 protocol OnboardingServiceProtocol {
     
+    func signUpNewUser(with email: String, password: String)
     func login(with userCredentials: UserCredentials, completionHandler: @escaping (Result<Void, Error>) -> Void)
     
 }
 
 final class OnboardingService: OnboardingServiceProtocol {
     
+    init() { //to jest potrzebne?
+        let handle = Auth.auth().addStateDidChangeListener { (auth, user) in
+            //...
+        }
+    }
+    
+    func signUpNewUser(with email: String, password: String) {
+        Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+            //...
+        }
+    }
+    
     func login(with userCredentials: UserCredentials, completionHandler: @escaping (Result<Void, Error>) -> Void) {
+        
+        Auth.auth().signIn(withEmail: userCredentials.login, password: userCredentials.password) { [weak self] authResult, error in
+            guard let strongSelf = self else {
+                return
+            }
+            //...
+        }
         
         // This body was written just to test OnboardingViewModel, it should be rewrite with OnboardingService task
         let login: String = "123"
