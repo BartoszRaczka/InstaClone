eafdd306bed9a7a5556e72b25f3a4eb715cd4338
Smallfish <piotr.rybka.pl@gmail.com>
2020-12-12 22:38:27 +0100

    [IC-55] Add basic logic to PhotoViewModel

--- InstaClone/Modules/PhotoModule/PhotoViewModel.swift
+++ InstaClone/Modules/PhotoModule/PhotoViewModel.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import UIKit
 
 protocol PhotoViewModelDelegate {
     
@@ -14,9 +15,26 @@ protocol PhotoViewModelDelegate {
 class PhotoViewModel {
     
     let delegate: PhotoViewModelDelegate?
+    private var capturedPhoto: UIImage!
+    var onRecapturePhotoAction: (() -> ())?
+    var contentService = ContentService()
     
     init(delegate: PhotoViewModelDelegate) {
         self.delegate = delegate
+    }
+    
+}
+
+// MARK: - Public methods
+extension PhotoViewModel {
+    
+    func capturePhotoButtonTapped(with photo: UIImage) {
+        self.capturedPhoto = photo
+        // TODO: Call contentService (when ContentService will be done)
+    }
+    
+    func recapturePhotoButtonTapped() {
+        self.onRecapturePhotoAction?() // This line allows PhotoView to recapturing photo
     }
     
 }
