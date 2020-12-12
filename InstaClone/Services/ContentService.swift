97fb584086307cde45a0c8fa9669d91c8bca964d
Bartosz Rączka <barteksae@gmail.com>
2020-12-12 22:04:51 +0100

    [IC-59] add pod Firebase/Storage

--- InstaClone/Services/ContentService.swift
+++ InstaClone/Services/ContentService.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import FirebaseStorage
 
 protocol ContentServiceProtocol {
     
@@ -13,4 +14,21 @@ protocol ContentServiceProtocol {
 
 final class ContentService: ContentServiceProtocol {
     
+    let storage = Storage.storage()
+    
+    func uploadRef() {
+        let randomID = UUID.init().uuidString
+        let uploadRef = storage.reference(withPath: "photos/\(randomID).jpg")
+        guard let imageData = imageView.image?.jpegData(compressionQuality: 0.75) else { return }
+        let uploadMetadata = StorageMetadata.init()
+        uploadMetadata.contentType = "image/jpeg"
+        
+        uploadRef.putData(imageData, metadata: uploadMetadata) { (downloadMetadata, error) in
+            if let error = error {
+                print("error")
+                return
+            }
+            print("\(String(downloadMetadata))")
+        }
+    }
 }
