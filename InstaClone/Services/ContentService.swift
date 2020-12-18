a6e12ab00228a9de6fc0bebf0be3cdb98fcb5d7e
Bartosz Rączka <barteksae@gmail.com>
2020-12-18 16:59:02 +0100

    [IC-59] add completionHandler

--- InstaClone/Services/ContentService.swift
+++ InstaClone/Services/ContentService.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import FirebaseAuth
 import FirebaseStorage
 
 protocol ContentServiceProtocol {
@@ -20,9 +21,15 @@ final class ContentService: ContentServiceProtocol {
         let imageID = UUID().uuidString
         let image = UIImage(named: "\()")
         let data = image?.pngData()
+        let userID = Auth.auth().currentUser?.uid
         
-        let reference = storage.reference(withPath: "users/\()/photos/\(imageID).png")
+        let reference = storage.reference(withPath: "users/\(userID)/photos/\(imageID).png")
         reference.putData(data, metadata: nil) { metadata, error in
+            if let error = error {
+                completionHandler(.failure(error))
+            } else {
+                completionHandler(.success(()))
+            }
         }
     }
 }
