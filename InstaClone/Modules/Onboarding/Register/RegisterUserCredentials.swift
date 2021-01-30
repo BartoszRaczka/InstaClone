c807e174cc1bfbb0eb9894d479c0343146389145
Bartosz Rączka <barteksae@gmail.com>
2021-01-30 22:48:34 +0100

    [IC-62]

---
+++ InstaClone/Modules/Onboarding/Register/RegisterUserCredentials.swift
@@ -0,0 +1,11 @@
+//
+//  RegisterUserCredentials.swift
+//  InstaClone
+//
+//  Created by MacBook on 29/01/2021.
+//
+
+struct RegisterUserCredentials {
+    var email: String = ""
+    var password: String = ""
+}
